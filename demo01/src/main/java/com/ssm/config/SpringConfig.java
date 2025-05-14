446960024ef160597118fde929101d0fd366a092
admin <augustipx@gmail.com>
2025-05-14 20:45:55 +0800

    基于注解实现spring3 ioc

---
+++ demo01/src/main/java/com/ssm/config/SpringConfig.java
@@ -0,0 +1,9 @@
+package com.ssm.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration  //=xml配置文件
+@ComponentScan("com.ssm")  //=<context:component-scan 标签
+public class SpringConfig {
+}
--- demo01/src/test/java/test01.java
+++ demo01/src/test/java/test01.java
@@ -1,5 +1,7 @@
+import com.ssm.config.SpringConfig;
 import com.ssm.dao.IUserDao;
 import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class test01 {
@@ -8,6 +10,13 @@ public class test01 {
 
 
         ClassPathXmlApplicationContext ioc = new ClassPathXmlApplicationContext("spring.xml");
+        IUserDao bean = ioc.getBean(IUserDao.class);
+        bean.getUser();
+    }
+
+    @Test
+    public void test2() {
+        AnnotationConfigApplicationContext ioc = new AnnotationConfigApplicationContext(SpringConfig.class);
         IUserDao bean = ioc.getBean(IUserDao.class);
         bean.getUser();
     }
